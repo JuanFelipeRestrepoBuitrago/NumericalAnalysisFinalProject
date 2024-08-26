e7d7d537088e5f7cbfe4f496bfb38fe0e126452b
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2024-08-26 09:30:38 -0500

    Main Project Structure Modified for Need

--- BACKEND/app/app.py
+++ BACKEND/app/app.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.requests import Request
-from asyncio import Lock
 
 # Routes and config modules import
 from app.api.config.env import API_NAME, PRODUCTION_SERVER_URL, DEVELOPMENT_SERVER_URL, LOCALHOST_SERVER_URL
@@ -14,21 +12,21 @@ from app.api.routes.routes import router
 
 from fastapi.openapi.utils import get_openapi
 
-
-class SynchronousMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app):
-        super().__init__(app)
-        self.lock = Lock()
-
-    async def dispatch(self, request: Request, call_next):
-        async with self.lock:
-            response = await call_next(request)
-        return response
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Actions to be executed during startup
+    print('API started')
+    
+    # Yield control to allow the app to run
+    yield
+    
+    # Actions to be executed during shutdown
+    print('API shut down')
 
 
 title = f'{API_NAME} API'
 description = f'{API_NAME} API description.'
-version = '1.0.0'
+version = '0.0.1'
 servers = [
     {"url": LOCALHOST_SERVER_URL, "description": "Localhost server"},
     {"url": DEVELOPMENT_SERVER_URL, "description": "Development server"},
@@ -36,7 +34,6 @@ servers = [
 ]
 contact = {
     'name': 'Juan Felipe Restrepo Buitrago',
-    'url': 'https://www.eafit.edu.co/',
     'email': 'jfrestrepb@eafit.edu.co',
 }
 license_info = {
@@ -55,8 +52,8 @@ app = FastAPI(
     version=version,
     contact=contact,
     license_info=license_info,
+    lifespan=lifespan
 )
-
 
 def custom_openapi():
     if app.openapi_schema:
@@ -69,11 +66,10 @@ def custom_openapi():
         servers=servers,
     )
     openapi_schema["info"]["x-logo"] = {
-        "url": "..."  # Add your logo URL here
+        "url": "..." # Add your logo URL here
     }
     app.openapi_schema = openapi_schema
     return app.openapi_schema
-
 
 app.openapi = custom_openapi
 
@@ -90,19 +86,6 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
-app.add_middleware(SynchronousMiddleware)
-
-
-@app.on_event('startup')
-async def on_startup():
-    # Actions to be executed when the API starts.
-    print('API started')
-
-
-@app.on_event('shutdown')
-async def on_shutdown():
-    # Actions to be executed when the API shuts down.
-    print('API shut down')
 
 # Include the routes
 app.include_router(router, prefix=f'/api/v1/{API_NAME}')
