f1014345e590d7dd16625bd6d81a946f8eea52e3
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2024-09-14 12:38:02 -0500

    Partial Pivot Implemented
    The partial pivot function was implemented and tested successfully, everything is working well so far

--- BACKEND/app/utils/linear_equation_systems.py
+++ BACKEND/app/utils/linear_equation_systems.py
@@ -3,7 +3,7 @@ from app.routes.routes import logger
 import numpy as np
 
 
-def regressive_substitution(Ab: np.array, n: int, logger=logger):
+def regressive_substitution(Ab: np.array, n: int):
     """
     This function performs the regressive substitution method to solve a system of equations.
     :param Ab: numpy array with the coefficients of the system of equations
@@ -29,3 +29,36 @@ def regressive_substitution(Ab: np.array, n: int, logger=logger):
         x[i] = (Ab[i, n] - sum) / Ab[i, i]
     # Return the solution
     return x
+
+
+def partial_pivot(Ab: np.array, n: int, k: int):
+    """
+    This function performs the partial pivot method to solve a system of equations.
+    :param Ab: numpy array with the coefficients of the system of equations
+    :param n: length of the system of equations
+    :param k: current iteration
+    :param logger: logger object
+    :return: numpy array with the coefficients of the system of equations after the partial pivot method
+    """
+    # Find the maximum value in the current column
+    max = np.abs(Ab[k, k])
+    max_row = k
+
+    # Iterate over the rows to find the maximum value
+    for s in range(k + 1, n):
+        # Check if the current value is greater than the maximum value
+        if np.abs(Ab[s, k]) > max:
+            max = np.abs(Ab[s, k])
+            max_row = s
+
+    # Check if the maximum value is zero
+    if max == 0:
+        raise_exception(SystemError("El sistema no tiene solución única"), logger)
+
+    # Swap the rows if the maximum value is not in the current row
+    if max_row != k:
+        # Swap the rows
+        Ab[[k, max_row]] = Ab[[max_row, k]]
+
+    # Return the coefficients of the system of equations after the partial pivot method
+    return Ab
